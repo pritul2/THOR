a33fe7d2693d9125ab5f321b4a872a0380109498
pritul dave <41751718+pritul2@users.noreply.github.com>
2020-04-14 18:02:25 +0530

    Update utils.py

--- trackers/SiamFC/utils.py
+++ trackers/SiamFC/utils.py
@@ -4,53 +4,82 @@
 # Written by Axel Sauer (axel.sauer@tum.de)
 # --------------------------------------------------------
 
+import torch
 import numpy as np
-import cv2
+from .utils import to_one_indexed, to_zero_indexed, crop_and_resize
+from .config import TrackerConfig
 
-def cxy_wh_2_rect(pos, sz):
-    return np.array([pos[0]-sz[0]/2, pos[1]-sz[1]/2, sz[0], sz[1]])  # 0-index
+def SiamFC_init(im, target_pos, target_sz, cfg):
+    state = {}
+    state['im_h'] = im.shape[0]
+    state['im_w'] = im.shape[1]
+    target_pos, target_sz = to_zero_indexed(target_pos, target_sz)
 
-def rect_2_cxy_wh(rect):
-    return np.array([rect[0]+rect[2]/2, rect[1]+rect[3]/2]), np.array([rect[2], rect[3]])  # 0-index
+    # set the tracker_config
+    p = TrackerConfig()
+    p.update(cfg)
 
-def crop_and_resize(image, center, size, out_size, pad_color):
-    # convert box to corners (0-indexed)
-    size = round(size)
-    corners = np.concatenate((
-        np.round(center - (size - 1) / 2),
-        np.round(center - (size - 1) / 2) + size))
-    corners = np.round(corners).astype(int)
+    # create hanning window
+    p.hann_window = np.outer(np.hanning(p.upscale_sz), np.hanning(p.upscale_sz))
+    p.hann_window /= p.hann_window.sum()
 
-    # pad image if necessary
-    pads = np.concatenate((
-        -corners[:2], corners[2:] - image.shape[:2]))
-    npad = max(0, int(pads.max()))
-    if npad > 0:
-        image = cv2.copyMakeBorder(
-            image, npad, npad, npad, npad,
-            cv2.BORDER_CONSTANT, value=pad_color)
+    # search scale factors
+    p.scale_factors = p.scale_step ** np.linspace(
+        -(p.scale_num // 2),
+        p.scale_num // 2, p.scale_num)
+    p.scale_factors_glob = np.array([0.5, 0.75, 1.0, 1.25, 1.75])
 
-    # crop image patch
-    corners = (corners + npad).astype(int)
-    patch = image[corners[0]:corners[2], corners[1]:corners[3]]
+    # exemplar image
+    avg_chans = np.mean(im, axis=(0, 1))
 
-    # resize to out_size
-    patch = cv2.resize(patch, (out_size, out_size))
+    # important params for later use
+    context = p.context * np.sum(target_sz)
+    p.z_sz = np.sqrt(np.prod(target_sz + context))
+    p.x_sz = p.z_sz * p.instance_sz / p.exemplar_sz
 
-    return patch
+    target_pos, target_sz = to_one_indexed(target_pos, target_sz)
 
-def to_zero_indexed(pos, sz):
-    # convert box to 0-indexed and center based [y, x, h, w]
-    box = cxy_wh_2_rect(pos, sz)
-    box = np.array([
-        box[1] - 1 + (box[3] - 1) / 2,
-        box[0] - 1 + (box[2] - 1) / 2,
-        box[3], box[2]], dtype=np.float32)
-    return box[:2], box[2:]
+    # fill the state dict
+    state['device'] = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    state['target_pos'] = target_pos
+    state['target_sz'] = target_sz
+    state['score'] = 1.0
+    state['p'] = p
+    state['avg_chans'] = avg_chans
+    return state
 
-def to_one_indexed(pos, sz):
-    box = np.array([
-        pos[1] + 1 - (sz[1] - 1) / 2,
-        pos[0] + 1 - (sz[0] - 1) / 2,
-        sz[1], sz[0]])
-    return rect_2_cxy_wh(box)
+def SiamFC_track(state, im, temp_mem):
+    p = state['p']
+    avg_chans = state['avg_chans']
+    inst_sz = p.instance_sz
+    scale_factors = p.scale_factors
+    old_pos, old_sz = to_zero_indexed(state['target_pos'], state['target_sz'])
+    dev = state['device']
+
+    # get instance images
+    ims = [crop_and_resize(
+        image=im, center=old_pos, size=p.x_sz * f,
+        out_size=inst_sz,
+        pad_color=avg_chans) for f in scale_factors]
+    ims = np.stack(ims, axis=0)
+    ims = torch.from_numpy(ims).to(dev).permute([0, 3, 1, 2]).float()
+
+    # track
+    target_pos, target_sz, score, scale = temp_mem.batch_evaluate(ims, old_pos, old_sz, p)
+
+    p.x_sz *= float(scale)
+    p.z_sz *= float(scale)
+
+    # return 1-indexed and left-top based bounding box
+    target_pos, target_sz = to_one_indexed(target_pos, target_sz)
+
+    target_pos[0] = max(0, min(state['im_w'], target_pos[0]))
+    target_pos[1] = max(0, min(state['im_h'], target_pos[1]))
+    target_sz[0] = max(10, min(state['im_w'], target_sz[0]))
+    target_sz[1] = max(10, min(state['im_h'], target_sz[1]))
+    state['target_pos'] = target_pos
+    state['target_sz'] = target_sz
+    state['score'] = score
+    state['p'] = p
+    state['crop'] = ims[1] # get non scaled image
+    return state
